0e656ce6a7baba5f4bb7e98f1d1151dc04ce55fe
sun4red <klzxcv1@gmail.com>
2024-01-25 22:57:22 +0900

    프로젝트 생성, 환경설정

---
+++ src/main/java/make/money/stock1/Stock1Application.java
@@ -0,0 +1,13 @@
+package make.money.stock1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stock1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stock1Application.class, args);
+	}
+
+}
